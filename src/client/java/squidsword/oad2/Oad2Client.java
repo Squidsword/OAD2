30b326ed67bdf5a314c0cffd784d8c2ba43cba8e
Max Everest <2maxeverest@gmail.com>
2024-11-29 13:04:48 -0500

    OAD2 Template

---
+++ src/client/java/squidsword/oad2/Oad2Client.java
@@ -0,0 +1,10 @@
+package squidsword.oad2;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Oad2Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
